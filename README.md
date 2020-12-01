6a6193f7cf6ffa27f6ec5eee0d1ba582a7adbda4
Wolfgang Risse <wri@uniteplus.de>
2020-12-01 10:59:30 +0100

    init 0

---
+++ README.md
@@ -0,0 +1,3 @@
+# collection of useful bash script elements
+
+- [create_file.sh](create_file.sh)
---
+++ create_file.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+cat > ~/file.txt <<EOF
+This is an example text.
+second line 
+third line
+...
+EOF
